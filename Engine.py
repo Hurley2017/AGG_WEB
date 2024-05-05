39d9960ff8c111ca602d01687d1c973999a21da5
Tusher Mondal <luciefer9062hurley@gmail.com>
2024-05-05 03:37:37 +0000

    turn u + p volme

--- Engine.py
+++ Engine.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 
 Application = Flask(__name__)
 
 @Application.route("/")
 def home():
-    return "Hello, World!"
+    return render_template("home.html")
 
 if __name__ == "__main__": 
-    Application.run()
+    Application.run(debug=True)
